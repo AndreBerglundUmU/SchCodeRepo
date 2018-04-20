d65747bb55ea7c569008625baf2fa812ce7ab607
André Berglund <andre.berglund@umu.se>
2018-04-20 16:45:21 +0200

    Now runs a simulation

--- workers.py
+++ workers.py
@@ -3,6 +3,14 @@ from scipy.fftpack import fft, ifft
 import numpy as np
 import supportFunctions as sf
 import schemes
+
+# import randomstate as rnd
+# Might be necessary for parallel rng generation
+# https://pypi.org/project/randomstate/1.10.1/
+
+# example
+# workers.simulation(L,M,T,N,sigma,u0Fun,schemes.PSLieSpl,[queries]) if queries is one query
+
 # import importlib
 # importlib.reload(...)
 
@@ -21,7 +29,7 @@ class Query(object):
 			self.timeStorageSize = desiredTimeStorageSize + 1
 	
 	def preallocateQueryResult(self):
-		return np.zeros(shape=(self.timeStorageSize,self.spaceStorageSize))
+		return np.zeros(shape=(self.timeStorageSize,self.spaceStorageSize),dtype=np.complex_)
 		
 
 def make_Query(function, spaceStorageSize, desiredTimeStorageSize,N):
@@ -30,29 +38,30 @@ def make_Query(function, spaceStorageSize, desiredTimeStorageSize,N):
 
 
 def simulation(L,M,T,N,sigma,u0,scheme,queries):
-	XInt = [-L,L];
-	TInt = [0,T];
-	h = (TInt[1]-TInt[0])/N;
-	dx = (XInt[1]-XInt[0])/M;
+	XInt = [-L,L]
+	TInt = [0,T]
+	h = (TInt[1]-TInt[0])/N
+	dx = (XInt[1]-XInt[0])/M
 	k = sf.createKVec(XInt[1],XInt[0],M)
 	kSq = np.power(k,2)
 	t = np.linspace(TInt[0],TInt[1],N+1)
 	x = np.linspace(XInt[0],XInt[1],M,endpoint=False)
 	
 	# Need to preallocate memory for query results
+	currU = u0(x)
 	queryStorage = []
 	queryIndex = []
 	for q in range(len(queries)):
-		queryStorage.append(queries[q].preallocateQueryResult)
-		queryStorage[q][0,:] = queries[q].function(u0)
+		queryStorage.append(queries[q].preallocateQueryResult())
+		queryStorage[q][0,:] = queries[q].function(currU)
 		queryIndex.append(1)
 	
-	currU = u0
 	for i in range(N):
+		dW = np.random.randn(2,1)*(h/2)
 		currU = scheme(currU,dW,k,kSq,h,sigma)
 		for q in range(len(queries)):
 			if (i % queries[q].periodicity) == (queries[q].periodicity - 1):
-				queryStorage[queryIndex[q],:] = queries[q].function(currU)
+				queryStorage[q][queryIndex[q],:] = queries[q].function(currU)
 				queryIndex[q] += 1
 			
 	return queryStorage
