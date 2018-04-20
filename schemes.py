662f22fa4005fcc7479fd869cc216f4372ed456c
André Berglund <andre.berglund@umu.se>
2018-04-20 12:41:39 +0200

    Translated all schemes and related support functions.

--- schemes.py
+++ schemes.py
@@ -1,135 +1,62 @@
 #! /usr/bin/env python
 from scipy.fftpack import fft, ifft
 import numpy as np
-#	IMPLICITSOLVING
-#def CrankNic(currU,dW,k,h):
-#def MidPoint(currU,dW,k,h):
-#def MidEul(currU,dW,k,h):
-#def SymExp(currU,dW,k,h):
+import supportFunctions as sf
 
-def cubicU(currU,sigma):
-	np.dot(np.power(np.absolute(currU),2*sigma),currU)
-
-def FEul(currU,dW,k,h,sigma):
-	a = (1 - 1j*np.sum(dW)*np.power(k,2));
+def PSFEul(currU,dW,k,kSq,h,sigma):
+	a = (1 - 1j*np.sum(dW)*kSq);
 	b = 1j*h;
-	nextU = np.dot(a,currU) + b*fft(cubicU(ifft(currU),sigma))
+	nextU = np.multiply(a,currU) + b*fft(sf.cubicU(ifft(currU),sigma))
 	return nextU
 
-#def BEul(currU,dW,k,h):
-#def StrangSpl(currU,dW,k,h):
-#def LieSpl(currU,dW,k,h):
-#def FourSpl(currU,dW,k,h):
-#def ExplExp(currU,dW,k,h):
+def PSBEul(currU,dW,k,kSq,h,sigma):
+	a = 1/(1 + 1j*np.sum(dW)*kSq);
+	b = 1j*h/(1 + 1j*np.sum(dW)*kSq);
+	nextU = np.multiply(a,currU) + np.multiply(b,fft(sf.cubicU(ifft(currU),sigma)))
+	return nextU
 
-
-
+def PSCN(currU,dW,k,kSq,h,sigma):
+	nextU = PSEulTypeSolver(currU,sum(dW),k,kSq,h,sigma,CNNonLin)
+	return nextU
 	
-#                    case 1 % Crank-Nicolson
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        if sigma == 1
-#                            CNG = @(u,v) (abs(u).^2+abs(v).^2).*(u+v)/4;
-#                        else
-#                            CNG = @(u,v) (abs(u).^(2*(sigma+1))-abs(v).^(2*(sigma+1)))./...
-#                                (abs(u).^2-abs(v).^2).*(u+v)/(2*(sigma+1));
-#                        end
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            currU = myPSImplSolver(currU,k,sum(dW),currh,CNG);
-#                        end
-#                    case 2 % Midpoint scheme
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        MG = @(u,v) abs((u+v)/2).^(2*sigma).*(u+v)/2;
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            currU = myPSImplSolver(currU,k,sum(dW),currh,MG);
-#                        end
-#                    case 3 % Theta Euler scheme, theta=0.5, version of midpoint
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        MG = @(u,v) abs(u).^(2*sigma).*u;
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            currU = myPSImplSolver(currU,k,sum(dW),currh,MG);
-#                        end
-#                    case 4 % Theta Euler scheme, theta=1, Explicit Euler
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            a = (1 - 1i*sum(dW)*k.^2);
-#                            b = 1i*currh;
-#                            currU = a.*currU + b.*fft(cubicU(ifft(currU)));
-#                        end
-#                    case 5 % Theta Euler scheme, theta=0, Implicit Euler
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            a = 1./(1 + 1i*sum(dW)*k.^2);
-#                            b = 1i*currh./(1 + 1i*sum(dW)*k.^2);
-#                            currU = a.*currU + b.*fft(cubicU(ifft(currU)));
-#                        end
-#                    case 6 % Theta splitting scheme, theta=0.5, Strang splitting
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            % Half first step
-#                            temp = exp(-dW(1)*1i*k.^2).*currU;
-#                            % Full nonlinear step
-#                            tempRealSpace = ifft(temp);
-#                            temp = fft(exp(currh*1i*abs(tempRealSpace).^(2*sigma)).*tempRealSpace);
-#                            % Half last step
-#                            currU = exp(-dW(2)*1i*k.^2).*temp;
-#                        end
-#                    case 7 % Theta splitting scheme, theta=1, Lie/Fourier splitting
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            % Full first step
-#                            tempRealSpace = ifft(exp(-sum(dW)*1i*k.^2).*currU);
-#                            % Full nonlinear step
-#                            currU = fft(exp(currh*1i*abs(tempRealSpace).^(2*sigma)).*tempRealSpace);
-#                        end
-#                    case 8 % Theta splitting scheme, theta=0, Lie/Fourier splitting
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            % Full nonlinear step
-#                            tempRealSpace = ifft(currU);
-#                           temp = fft(exp(currh*1i*abs(tempRealSpace).^(2*sigma)).*tempRealSpace);
-#                            % Full first step
-#                            currU = exp(-sum(dW)*1i*k.^2).*temp;
-#                        end
-#                    case 9 % Explicit exponential scheme
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            % Full first step
-#                            currU = exp(-sum(dW)*1i*k.^2).*currU + 1i*currh*exp(-sum(dW)*1i*k.^2).*fft(cubicU(ifft(currU)));
-#                        end
-#                    case 10 % Symmetric exponential scheme
-#                        % Initialization
-#                        currU = fft(u0FunVal);
-#                        for i = 1:currN
-#                            dW = W(i,:);
-#                            %% Scheme calculations
-#                            NStar = myPSNStarSolver(currU,k,dW(1),currh,sigma);
-#                            currU = exp(-sum(dW)*1i*k.^2).*currU + currh*exp(-dW(2)*1i*k.^2).*NStar;
-#                        end
-#                end
+def PSMEul(currU,dW,k,kSq,h,sigma):
+	nextU = PSEulTypeSolver(currU,sum(dW),k,kSq,h,sigma,MEulNonLin)
+	return nextU
+	
+def PSMP(currU,dW,k,kSq,h,sigma):
+	nextU = PSEulTypeSolver(currU,sum(dW),k,kSq,h,sigma,MPNonLin)
+	return nextU
+	
+def PSStrangSpl(currU,dW,k,kSq,h,sigma):
+	# Half derivative step
+	tempPhysCurrU = ifft(np.multiply(np.exp(-dW[0]*1j*kSq),currU))
+	# Full nonlinear step
+	tempU = fft(np.multiply(np.exp(h*1j*np.power(np.abs(tempPhysCurrU),2*sigma)),tempPhysCurrU))
+	# Half derivative step
+	nextU = np.multiply(np.exp(-dW[1]*1j*kSq),tempU)
+	return nextU
+	
+def PSLieSpl(currU,dW,k,kSq,h,sigma):
+	# Full derivative step
+	tempPhysCurrU = ifft(np.multiply(np.exp(-sum(dW)*1j*kSq),currU))
+	# Full nonlinear step
+	nextU = fft(np.multiply(np.exp(h*1j*np.power(np.abs(tempPhysCurrU),2*sigma)),tempPhysCurrU))
+	return nextU
+	
+def PSFourSpl(currU,dW,k,kSq,h,sigma):
+	# Full nonlinear step
+	tempPhysCurrU = ifft(currU)
+	tempU = fft(np.multiply(np.exp(h*1j*np.power(np.abs(tempPhysCurrU),2*sigma)),tempPhysCurrU))
+	# Full derivative step
+	nextU = np.multiply(np.exp(-sum(dW)*1j*kSq),tempU)
+	return nextU
+	
+def PSExplExp(currU,dW,k,kSq,h,sigma):
+	nextU = np.multiply(np.exp(-sum(dW)*ij*kSq),currU) + 1j*h*np.multiply(np.exp(-sum(dW)*ij*kSq),fft(sf.cubicU(ifft(currU))))
+	return nextU
+	
+def PSSymExp(currU,dW,k,kSq,h,sigma):
+	kSquared = kSq;
+	NStar = PSNStarSolver(currU,dW[0],k,kSq,h,sigma)
+	currU = np.multiply(np.exp(-sum(dW)*1j*kSq),currU) + np.multiply(h*np.exp(-dW[1]*1j*kSq),NStar)
+	return nextU
--- supportFunctions.py
+++ supportFunctions.py
@@ -1,32 +1,101 @@
 #! /usr/bin/env python
 from scipy.fftpack import fft, ifft
 import numpy as np
-#	IMPLICITSOLVING
-#def CrankNic(currU,dW,k,h):
-#def MidPoint(currU,dW,k,h):
-#def MidEul(currU,dW,k,h):
-#def SymExp(currU,dW,k,h):
-
-def createKVec(leftPoint,rightPoint,numModes):
-	halfNumber = numModes
-	posModes = np.array(range(halfNumber))
-	negModes = np.flip(-np.array(halfNumber))
-	tempVec = np.append(posModes,no.arange(1))
-	tempVec = np.append(tempVec,negModes)
-	k = 2*pi/(rightPoint-leftPoint)*tempVec
-	#k = 2*pi/(rightPoint-leftPoint)*[0:numModes/2-1, 0, -numModes/2+1:-1];
-
+	
 def cubicU(currU,sigma):
-	np.dot(np.power(np.absolute(currU),2*sigma),currU)
+	return np.multiply(np.power(np.absolute(currU),2*sigma),currU)
+	
+def createKVec(leftPoint,rightPoint,numModes):
+	halfNumber = int(numModes/2)
+	posModes = np.array(range(halfNumber))
+	negModes = np.flip(-np.array(range(1,halfNumber)),0)
+	tempVec = np.append(posModes,np.arange(1))
+	tempVec = np.append(tempVec,negModes)
+	return 2*np.pi/(rightPoint-leftPoint)*tempVec
 
-def FEul(currU,dW,k,h,sigma):
-	a = (1 - 1j*np.sum(dW)*np.power(k,2));
-	b = 1j*h;
-	nextU = np.dot(a,currU) + b*fft(cubicU(ifft(currU),sigma))
+def PSEulTypeSolver(currU,dW,k,kSq,h,sigma,G):
+	# Initialize loop variables
+	crit = true;
+	i = 1;
+	nextU = currU;
+	realSpaceCurrU = ifft(currU);
+	realSpaceNextU = realSpaceCurrU;
+	# Calculate some values which don't change
+	a = np.multiply(
+		np.divide(1 - 1j*dW/2*kSq,
+			1 + 1j*dW/2*kSq),
+		currU)
+	b = 1j*h/(1 + 1j*dW/2*kSq);
+
+	while crit & i < 120:
+		tempU = nextU;
+		nextU = a + np.multiply(b,fft(G(realSpaceCurrU,realSpaceNextU,sigma)));
+		realSpaceNextU=ifft(nextU);
+		crit = np.linalg.norm(tempU-nextU) > np.spacing(1);
+		i = i+1;
+	end
 	return nextU
 
-#def BEul(currU,dW,k,h):
-#def StrangSpl(currU,dW,k,h):
-#def LieSpl(currU,dW,k,h):
-#def FourSpl(currU,dW,k,h):
-#def ExplExp(currU,dW,k,h):
+def PSNStarSolver(currU,firstHalfdW,k,kSq,h,sigma):
+	# Initialize loop variables
+	crit = true
+	i = 1
+	NStar = currU
+	# Calculate some values which don't change
+	a = np.multiply(
+		np.exp(-firstHalfdW*1j*kSq),
+		currU)
+
+	while crit & i < 120:
+		oldNStar = NStar
+		tempNStar = a + h/2*NStar
+		NStar = fft(cubicU(ifft(tempNStar)))
+		crit = np.linalg.norm(oldNStar-NStar) > np.spacing(1);
+		i = i+1;
+	end
+	return nextU
+	
+def CNNonLin(u,v,sigma):
+	if sigma == 1:
+		retVal = np.multiply(
+			np.power(np.abs(u),2) + 
+			np.power(np.abs(v),2),
+			u+v)/(2*(sigma+1))
+	elif sigma == 2:
+		retVal = np.multiply(
+			np.power(np.abs(u),4) + 
+			np.multiply(np.power(np.abs(u),2),np.power(np.abs(v),2)) + 
+			np.power(np.abs(v),4),
+			u+v)/(2*(sigma+1))
+	elif sigma == 3:
+		retVal = np.multiply(
+			np.power(np.abs(u),6) + 
+			np.multiply(np.power(np.abs(u),4),np.power(np.abs(v),2)) + 
+			np.multiply(np.power(np.abs(u),2),np.power(np.abs(v),4)) + 
+			np.power(np.abs(v),6),
+			u+v)/(2*(sigma+1))
+	elif sigma == 4:
+		retVal = np.multiply(
+			np.power(np.abs(u),8) + 
+			np.multiply(np.power(np.abs(u),6),np.power(np.abs(v),2)) + 
+			np.multiply(np.power(np.abs(u),4),np.power(np.abs(v),4)) + 
+			np.multiply(np.power(np.abs(u),2),np.power(np.abs(v),6)) + 
+			np.power(np.abs(v),8),
+			u+v)/(2*(sigma+1))
+	else:
+		retVal = np.multiply(
+			np.divide(
+				np.power(np.abs(u),2*(sigma+1)) - np.power(np.abs(v),2*(sigma+1)),
+				np.power(np.abs(u),2) - np.power(np.abs(v),2) + pow(np.spacing(0),1/2)), # Adding square root of smallest value to interpret as nonzero
+			u+v)/(2*(sigma+1))
+	return retVal
+
+def MEulNonLin(u,v,sigma):
+	retVal = cubicU(u)
+	return retVal
+
+def MPNonLin(u,v,sigma):
+	retVal = cubicU((u+v)/2)
+	return retVal
+
+
