0a0db5123d551dae7529c3d6008a6547122584c6
André Berglund <andre.berglund@umu.se>
2018-04-27 15:45:53 +0200

    Done with waterfall plot and multiple queries.

--- plotEvolutionFirstTest.py
+++ plotEvolutionFirstTest.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, ifft
 from sympy import *
+import time
 import schroed_schemes as ss
 import schroed_functions as sf
 import misc_functions as mf
 import plot_functions, query_simulation
 
 # Parameters
-sigma = 1
+sigma = 4
 L = 30
 M = 2**10
 T = 1
 N = 2**10
+# N = 2^19 for Debussche (T=1)
+# M = 2^29 for Debussche
 storedTime = 2**8
 
 # Derived values
@@ -23,6 +26,7 @@ dx = (XInt[1]-XInt[0])/M
 k = mf.createKVec(XInt[1],XInt[0],M)
 kSq = np.power(k,2)
 t = np.linspace(TInt[0],TInt[1],N+1)
+stored_t = np.linspace(TInt[0],TInt[1],storedTime+1)
 x = np.linspace(XInt[0],XInt[1],M,endpoint=False)
 
 # Initial function
@@ -32,11 +36,12 @@ alpha = 1
 c = 1
 sech_fun = lambda x: 2/(np.exp(x) + np.exp(-x))
 u0 = lambda x: np.sqrt(2*alpha/q)*np.multiply(np.exp(0.5*1j*c*x),sech_fun(np.sqrt(alpha)*x))
+u0 = lambda x: 1.4*np.exp(-3*np.power(x,2))
 
 u0FunVal = fft(u0(x))
 
 # Scheme
-scheme = ss.PSSymExp
+scheme = ss.PSLieSpl
 
 # PSFEul
 # PSBEul
@@ -50,12 +55,18 @@ scheme = ss.PSSymExp
 # PSSymExp
 
 # Query construction (query, size of return, number of time steps stored, total number of time steps)
-my_query = query_simulation.make_query(lambda x: x, M, storedTime, N)
+my_query = query_simulation.make_query(lambda x: ifft(x), M, storedTime, N)
+my_query2 = query_simulation.make_query(lambda x: mf.L2Norm(ifft(x),dx), 1, storedTime, N)
+norm_string = 'L2 norm'
 
 # Generating Brownian motion and running the simulation
 dW = np.random.randn(2,N)*np.sqrt(h/2)
-result = query_simulation.pseudospectral_simulation(N,h,k,kSq,sigma,u0FunVal,dW,scheme,[my_query])
+result = query_simulation.pseudospectral_simulation(N,h,k,kSq,sigma,u0FunVal,dW,scheme,[my_query,my_query2])
 
-
-# Plot over time
-plot_functions.plot_physical_evolution(result,x,t,dW,storedTime,N,L)
+# Different plot tests
+l2_axis_args = [stored_t[0], stored_t[-1], 0, 1.1*np.max(result[1][:,0])]
+plot_functions.plot_waterfall(np.abs(result[0]),x,stored_t)
+time.sleep(3)
+plot_functions.plot_norm_evolution(result[1][:,0],stored_t,l2_axis_args,norm_string)
+time.sleep(1)
+#plot_functions.plot_physical_evolution(result[0],x,t,dW,storedTime,N,L)
--- plot_functions.py
+++ plot_functions.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+from matplotlib.collections import PolyCollection
+from mpl_toolkits.mplot3d import axes3d
 from scipy.fftpack import fft, ifft
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,12 +16,52 @@ import validation_functions as vf
 # import importlib
 # importlib.reload(...)
 
+def plot_waterfall(function_evolution,x,stored_t):
+# https://stackoverflow.com/questions/31189665/plotting-using-polycollection-in-matplotlib
+	# fun_val = np.abs(function_evolution)
+	fig = plt.figure(figsize=(15,15))
+	ax = fig.add_subplot(111, projection='3d')
+	verts = []
+	for i in range(len(stored_t)):
+		xs = np.concatenate([[x[0]],x, [x[-1]]])
+		ys = np.concatenate([[0],function_evolution[i,:],[0]])
+		verts.append(list(zip(xs, ys)))
+	poly = PolyCollection(verts, facecolors=(1,1,1,1), edgecolors=(0,0,1,1))
+
+	# The zdir keyword makes it plot the "z" vertex dimension (radius)
+	# along the y axis. The zs keyword sets each polygon at the
+	# correct radius value.
+	ax.add_collection3d(poly, zs=stored_t, zdir='y')
+
+	ax.set_xlim3d(x.min(), x.max())
+	ax.set_xlabel('x')
+	ax.set_ylim3d(stored_t.min(), stored_t.max())
+	ax.set_ylabel('t')
+	ax.set_zlim3d(function_evolution.min(), function_evolution.max())
+	ax.set_zlabel('|u|')
+	#plt.show()
+	fig.canvas.draw()
+	plt.savefig('waterfall2.pdf')
+	return fig
+	
+def plot_norm_evolution(norm_evolution,stored_t,axis_args,norm_string):
+	vf.is_vector(norm_evolution)
+	# vf.is_matrix(function_evolution) # function_evolution is not a numpy matrix
+	fig = plt.figure(figsize=(15,15))
+	plt.plot(stored_t,np.real(norm_evolution))
+	plt.axis(axis_args)
+	plt.xlabel('t')
+	plt.ylabel(norm_string)
+	#plt.show()
+	plt.savefig('plot2.pdf')
+	return fig
+	
 def plot_physical_evolution(function_evolution,x,t,dW,storedTime,N,L):
 	vf.is_vector(x)
 	# vf.is_matrix(function_evolution) # function_evolution is not a numpy matrix
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	plotVal = ifft(function_evolution[0][0,:])
+	plotVal = function_evolution[0,:]
 	axis_ceil = 1.1*np.max(np.abs(plotVal))
 	line = ax.plot(x, np.real(plotVal),'b', x, np.imag(plotVal),'r', x, np.abs(plotVal), 'y')
 
@@ -30,10 +72,11 @@ def plot_physical_evolution(function_evolution,x,t,dW,storedTime,N,L):
 		t_val = t[index]
 		W_val = W[index]
 		
-		plotVal = ifft(function_evolution[0][i+1,:])
+		plotVal = function_evolution[i+1,:]
 		line[0].set_ydata(np.real(plotVal))
 		line[1].set_ydata(np.imag(plotVal))
 		line[2].set_ydata(np.abs(plotVal))
+		plt.legend(line, ['real(u)','imag(u)','abs(u)'])
 		#plt.plot(x, np.real(plotVal),'b', x, np.imag(plotVal),'r', x, np.abs(plotVal), 'y')
 		plt.axis([-L, L, -axis_ceil, axis_ceil])
 		plt.suptitle('t = {:1.4f}, W_t = {:4.4f}'.format(t_val,W_val))#,fontsize=20)
